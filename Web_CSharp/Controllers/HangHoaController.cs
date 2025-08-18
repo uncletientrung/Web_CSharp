810d3171b638280944133553ab83780a5a1acf87
uncletientrung <thihachcf@gmail.com>
2025-08-18 18:50:00 +0700

    add db vao category, product

--- Web_CSharp/Controllers/HangHoaController.cs
+++ Web_CSharp/Controllers/HangHoaController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web_CSharp.Data;
+using Web_CSharp.ViewModels;
 
 namespace Web_CSharp.Controllers
 {
     public class HangHoaController : Controller
     {
+        private readonly Hshop2023Context db;
+
         public IActionResult Index(int? loai)
         {
-            return View();
+            var hangHoas = db.HangHoas.AsQueryable();
+            if (loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(p => p.MaLoai == loai.Value);
+            }
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai // ????? MaLoaiNavigation là gì
+            });
+            return View(result);
+        }
+
+        public HangHoaController(Hshop2023Context context)
+        {
+            db = context;
         }
     }
 }
---
+++ Web_CSharp/ViewModels/HangHoaVM.cs
@@ -0,0 +1,13 @@
+﻿namespace Web_CSharp.ViewModels
+{
+    public class HangHoaVM
+    {
+        public int MaHh { get; set; }
+        public string TenHH { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public string MoTaNgan { get; set; }
+        public string TenLoai { get; set; }
+
+    }
+}
