aab90cc465f117a2a60c38194edde2c6bad306d4
uncletientrung <thihachcf@gmail.com>
2025-08-18 19:44:33 +0700

    Hoan thien nut Searcg

--- Web_CSharp/Controllers/HangHoaController.cs
+++ Web_CSharp/Controllers/HangHoaController.cs
@@ -7,6 +7,11 @@ namespace Web_CSharp.Controllers
     public class HangHoaController : Controller
     {
         private readonly Hshop2023Context db;
+        public HangHoaController(Hshop2023Context context)
+        {
+            db = context;
+        }
+
 
         public IActionResult Index(int? loai)
         {
@@ -26,10 +31,25 @@ namespace Web_CSharp.Controllers
             });
             return View(result);
         }
-
-        public HangHoaController(Hshop2023Context context)
+        public IActionResult Search(string? query)
         {
-            db = context;
+            var hangHoas = db.HangHoas.AsQueryable();
+            if (query !=null)
+            {
+                hangHoas = hangHoas.Where(p => p.TenHh.Contains(query));
+            }
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai // ????? MaLoaiNavigation là gì
+            });
+            return View(result);
         }
+
+
     }
 }
